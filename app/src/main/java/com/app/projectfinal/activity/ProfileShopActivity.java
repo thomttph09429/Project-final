9be167340a550377a696b50d928e63e510521167
ThomTT1 <truongthithom1999@gmail.com>
2022-09-21 15:49:50 +0700

    update

---
+++ app/src/main/java/com/app/projectfinal/activity/ProfileShopActivity.java
@@ -0,0 +1,14 @@
+package com.app.projectfinal.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_shop);
+    }
+}
